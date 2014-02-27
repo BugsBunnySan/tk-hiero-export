2458b98d18c09e214c47e5ca0324ad55ce6ac22b
Sebastian Haas <shaas@egg.ie>
2014-02-27 14:47:12 +0000

    fix wrong cache module name

--- hooks/hiero_resolve_custom_strings.py
+++ hooks/hiero_resolve_custom_strings.py
@@ -7,24 +7,24 @@ class HieroResolveCustomStrings(Hook):
 
     def _formatSequenceString(self, keyword, task, sg_shot):
         sg_sequence = None
-        if sg_shot['sg_sequence']['id'] not in ShotgunHieroObjectBase._sg_lookup_cache['Sequence']:
+        if sg_shot['sg_sequence']['id'] not in HieroResolveCustomString._sg_lookup_cache['Sequence']:
             sg_sequence = find_one_entity_by_id('Sequence', sg_shot['sg_sequence']['id'], fields=['code'])
             if not sg_sequence:
                 return ''
-            ShotgunHieroObjectBase._sg_lookup_cache['Sequence'][sg_shot['sg_sequence']['id']] = sg_sequence
+            HieroResolveCustomString._sg_lookup_cache['Sequence'][sg_shot['sg_sequence']['id']] = sg_sequence
         else:
-            sg_sequence = ShotgunHieroObjectBase._sg_lookup_cache['Sequence'][sg_shot['sg_sequence']['id']]
+            sg_sequence = HieroResolveCustomString._sg_lookup_cache['Sequence'][sg_shot['sg_sequence']['id']]
         return sg_sequence['code']
         
     def _formatSceneString(self, keyword, task, sg_shot):
         sg_scene = None
-        if sg_shot['sg_scene']['id'] not in ShotgunHieroObjectBase._sg_lookup_cache['Scene']:
+        if sg_shot['sg_scene']['id'] not in HieroResolveCustomString._sg_lookup_cache['Scene']:
             sg_scene = find_one_entity_by_id('Scene', sg_shot['sg_scene']['id'], fields=['code'])
             if not sg_scene:
                 return ''
-            ShotgunHieroObjectBase._sg_lookup_cache['Scene'][sg_shot['sg_scene']['id']] = sg_scene
+            HieroResolveCustomString._sg_lookup_cache['Scene'][sg_shot['sg_scene']['id']] = sg_scene
         else:
-            sg_scene = ShotgunHieroObjectBase._sg_lookup_cache['Scene'][sg_shot['sg_scene']['id']]
+            sg_scene = HieroResolveCustomString._sg_lookup_cache['Scene'][sg_shot['sg_scene']['id']]
         return sg_scene['code']        
 
     def _formatShotNoString(self, keyword, task, sg_shot):
@@ -33,11 +33,11 @@ class HieroResolveCustomStrings(Hook):
 
     def execute(self, task, keyword, **kwargs):
         lookup_key = task._item.name()
-        if lookup_key in ShotgunHieroObjectBase._sg_lookup_cache['Shot']:
-            sg_shot = ShotgunHieroObjectBase._sg_lookup_cache['Shot'][lookup_key]
+        if lookup_key in HieroResolveCustomString._sg_lookup_cache['Shot']:
+            sg_shot = HieroResolveCustomString._sg_lookup_cache['Shot'][lookup_key]
         else:
             sg_shot = find_one_entity_by_code("Shot", task._item.name(), self.parent.app.get_setting("custom_template_fields"))
-            ShotgunHieroObjectBase._sg_lookup_cache['Shot'][lookup_key] = sg_shot
+            HieroResolveCustomString._sg_lookup_cache['Shot'][lookup_key] = sg_shot
         if not sg_shot:
             return ''            
         if keyword == '{sg_sequence}':
